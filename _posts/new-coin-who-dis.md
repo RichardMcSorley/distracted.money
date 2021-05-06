23ad32c89d950d44f008db73611f9475ba527d30
Richard McSorley <Richard.McSorley@hey.com>
2021-05-06 02:14:27 -0400

    init

---
+++ _posts/new-coin-who-dis.md
@@ -0,0 +1,12 @@
+---
+title: 'New Coin Who Dis' 
+excerpt: DISTRACTED coin | A new crypto currency for a new age.
+date: '2021-06-06T06:46:53Z'
+# date -u +"%Y-%m-%dT%H:%M:%SZ"
+author:
+  name: Richard McSorley
+---
+
+## Introducing DISTRACTED (Distracted Boyfriend) Coin 
+
+The coin that you don't tell your girlfriend about.
---
+++ pages/api/feed.jsx
@@ -0,0 +1,49 @@
+import allPosts from '../../public/posts.json'
+import moment from 'moment';
+
+const blogPostsRssXml = posts => {
+    const latestPostDate = posts[0].date; 
+    let rssItemsXml = "";
+    posts.forEach(post => {
+        rssItemsXml += `
+      <item>
+        <title>${post.title}</title>
+        <link>
+          ${`https://distracted.money/posts/${post.slug}`}
+        </link>
+        
+        <pubDate>${post.date}</pubDate>
+        <description>
+        <![CDATA[${post.excerpt}]]>
+        </description>
+        <enclosure url="https://richardmcsorley.com${post.coverImage}" type="image/jpeg"/>
+        <author>${post.author.name}</author>
+    </item>`;
+    });
+    return {
+        rssItemsXml,
+        latestPostDate
+    };
+};
+
+const getRssXml = blogPosts => {
+    const { rssItemsXml, latestPostDate } = blogPostsRssXml(blogPosts);
+    return `<?xml version="1.0" ?>
+  <rss version="2.0">
+    <channel>
+        <title>A cryptocurrency by Richard McSorley</title>
+        <link>https://distracted.money</link>
+        <description>A coin of sorts.</description>
+        <language>en</language>
+        <lastBuildDate>${latestPostDate}</lastBuildDate>
+        ${rssItemsXml}
+    </channel>
+  </rss>`;
+};
+
+export default function Feed(req, res){
+    res.setHeader("Content-Type", "text/xml");
+        res.write(getRssXml(allPosts));
+        res.end();
+}
+
