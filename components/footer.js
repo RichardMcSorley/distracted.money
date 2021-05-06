59517e74d985a292ef100b9c40596bdaf4e2febe
Richard McSorley <Richard.McSorley@hey.com>
2021-05-06 02:59:26 -0400

    updates

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="p-3 mt-10 text-center bg-gray-800 flex-column">
       <Container>
           <div className="flex flex-row justify-evenly">
-        <p>Copyright © 2021  Richard McSorley. All rights reserved.</p>
+            <p>Copyright © 2021  Richard McSorley. All rights reserved. <a href="https://etherscan.io/token/0x351016d3ec753db8e98a783cf51c8d6a4a8af151">Token Contract</a></p>
           </div>
       </Container>
     </footer>
